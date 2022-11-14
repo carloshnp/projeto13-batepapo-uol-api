ad5723fada626813ad482e4668f68d5b3aebe1cc
carloshnp <carloshnpwork@gmail.com>
2022-11-14 11:21:58 -0300

    feat: mongo client connections + db collections

--- src/app.js
+++ src/app.js
@@ -10,9 +10,16 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONG0_URI)
 let db;
-mongoClient.connect(() => {
-    db = mongoClient.db('projeto13_batepapo_uol_api')
-})
+
+try {
+  await mongoClient.connect()
+} catch (err) {
+  console.log(err)
+}
+
+db = mongoClient.db('projeto13_batepapo_uol_api')
+const usersCollection = db.collection('users')
+const msgCollection = db.collection('msg')
 
 app.listen(5000, () => {
   console.log("App running at port: 5000");
